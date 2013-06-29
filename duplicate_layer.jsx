754a07faf3953a00558fd84c8339e2de557ec257
fabiantheblind <fabiantheblind@googlemail.com>
2013-06-29 17:04:12 +0200

    some more snippets

---
+++ duplicate_layer.jsx
@@ -0,0 +1,1 @@
+﻿fun();function fun(){app.beginUndoGroup("XXX");var curComp = app.project.activeItem;   if (!curComp || !(curComp instanceof CompItem)){        alert("noComp");        return;    };var num = 99;for(var i = 0; i < num; i ++){        curComp.selectedLayers[0].duplicate();    }app.endUndoGroup();}
