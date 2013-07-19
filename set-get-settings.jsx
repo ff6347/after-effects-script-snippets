16e66978f4899247a9f6d47e78b2d35bc9f75ed7
fabiantheblind <fabiantheblind@googlemail.com>
2013-07-19 15:21:12 +0200

    added submodule for SettingsControl Class

--- set-get-settings.jsx
+++ set-get-settings.jsx
@@ -33,6 +33,7 @@
 
 // read write settings
 
+#include "AESettingsControl/SettingsControl.jsx";
 
 var data = {
   settingsSectionName:"testSettings",
@@ -41,6 +42,11 @@ var data = {
   key_float:"afloatingpointvalue",
   key_boolean:"aboolean"
 };
+
+// the settings control class is a own submodule
+// you can find it here
+// https://github.com/fabiantheblind/AESettingsControl
+//
 var sc = new SettingsControl(data.settingsSectionName);
 test_set();
 test_get();
@@ -50,7 +56,6 @@ sc = null;
 function test_get () {
   var resbool = sc.exec.get_setting_boolean( data.key_boolean);
   alert("Boolean Value: "+resbool);
-
   var resint = sc.exec.get_setting_int( data.key_int);
   alert("Integer Value: "+ resint);
 
@@ -66,131 +71,6 @@ function test_set(){
    sc.exec.set_setting_number( data.key_int, 345);
    sc.exec.set_setting_string(data.key_string, "Hello Settings");
   sc.exec.set_setting_number(data.key_float, 2.5);
-}
-
-
-/**
- * This is a settings control class
- * Used it at will
- * @param {String} _section the Settings Section Name
- */
-function SettingsControl (_section) {
-/**
- * Usage:
- *
- *  var data = {
- *   settingsSectionName:"testSettings",
- *   key_string:"astring",
- *   key_int:"ainteger",
- *   key_float:"afloatingpointvalue",
- *   key_boolean:"aboolean"
- * };
- *
- * var sc = new SettingsControl(data.settingsSectionName);
- *
- * test_set();
- * test_get();
- * sc = null;
- *
- *
- *
- * function test_get () {
- *   var resbool = sc.exec.get_setting_boolean( data.key_boolean);
- *   alert("Boolean Value: "+resbool);
- *
- *   var resint = sc.exec.get_setting_int( data.key_int);
- *   alert("Integer Value: "+ resint);
- *
- *     var resfloat = sc.exec.get_setting_int( data.key_float);
- *   alert("Floating Point Value: "+ resfloat);
- *
- *     var resstring = sc.exec.get_setting_string( data.key_string);
- *   alert("String Value: "+ resstring);
- * }
- * function test_set(){
- *    sc.exec.set_setting_boolean( data.key_boolean, false);
- *    sc.exec.set_setting_number( data.key_int, 345);
- *    sc.exec.set_setting_string(data.key_string, "Hello Settings");
- *   sc.exec.set_setting_number(data.key_float, 2.5);
- * }
- *
- */
-
-  this.section = _section;
-  this.exec = {
-  section : _section,
-/**
- * GETTER functions
- */
-"get_setting"  : function (key, variable) {
-  if((app.settings.haveSetting(this.section,key) === true)){
-    variable = app.settings.getSetting(this.section, key);
-  }
-  return variable;
-},
-
-"get_setting_int" : function ( key) {
-  var integer = null;
-  integer = this.get_setting( key, integer);
-  if(integer !== null){
-      integer = parseInt(integer,10);
-      if(isNaN(integer)){
-        integer = null;
-      }
-  }
-  return integer;
-},
-
-"get_setting_float"  : function ( key) {
-  var floatingpointvalue = null;
-  floatingpointvalue = this.get_setting( key, floatingpointvalue);
-  if(floatingpointvalue !== null){
-      floatingpointvalue = parseFloat(floatingpointvalue);
-      if(isNaN(floatingpointvalue)){
-        floatingpointvalue = null;
-      }
-  }
-  return floatingpointvalue;
-},
-
-"get_setting_boolean"  : function ( key) {
-  var bool = null;
-  bool = this.get_setting( key, bool);
-  if(bool !== null){
-    bool = (bool === 'true') ? true : false;
-  }
-  return bool;
-},
-
-"get_setting_string"  : function ( key) {
-  var str = null;
-  str = this.get_setting( key, str);
-  return str;
-},
-
-
-/**
- * SETTER FUNCTIONS
- */
-
-
-"set_setting" : function ( key, val){
-    app.settings.saveSetting(this.section, key, val);
-},
-
-"set_setting_number"  : function ( key, value) {
-  this.set_setting( key, (String(value)).toLowerCase());
-},
-
-"set_setting_string"  : function ( key, value) {
-  this.set_setting( key, String(value));
-},
- set_setting_boolean  : function ( key, value) {
-   var val = value ? 'true' : 'false';
-   this.set_setting( key , (value ? 'true' : 'false'));
- }
-};
-
 }
 
 
